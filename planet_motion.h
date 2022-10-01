a197c3bc90e665a09b6dfd5d448b35dd6bda4d6e
Phillip Stevens <phillip.stevens@gmail.com>
2022-10-02 10:06:17 +1100

    add licence and C++ exclusion

--- planet_motion.h
+++ planet_motion.h
@@ -1,3 +1,37 @@
+/*
+ * planet_motion.h
+ *
+ * Copyright (c) 2021 Phillip Stevens
+ * Create Time: July 2021
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted,free of charge,to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),to deal
+ * in the Software without restriction,including without limitation the rights
+ * to use,copy,modify,merge,publish,distribute,sublicense,and/or sell
+ * copies of the Software,and to permit persons to whom the Software is
+ * furnished to do so,subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS OR
+ * IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,DAMAGES OR OTHER
+ * LIABILITY,WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE,ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+
+#ifndef _PLANET_MOTION_H
+#define _PLANET_MOTION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // numeric constants...
 
@@ -63,4 +97,10 @@ void addCartesianCoordinates ( cartesian_coordinates_t * base, const cartesian_c
 // utility functions (C or assembly)
 
 FLOAT rev (FLOAT x) __z88dk_fastcall;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _PLANET_MOTION_H  */
 
